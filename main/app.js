4865bbcafe021143cb712c2230f973f2c81ee118
Ubuntu <ubuntu@ip-172-31-19-21.us-east-2.compute.internal>
2019-10-30 15:27:56 +0000

    Salvare 1

--- main/app.js
+++ main/app.js
@@ -1,7 +1,23 @@
 function distance(first, second){
 	//TODO: implementați funcția
 	// TODO: implement the function
-}
 
+	var c1 = Object.keys(first).length;
+	var c2 = Object.keys(second).length;
+
+	if(!(first instanceof Array) || !(second instanceof Array))
+		{
+			throw new Error("InvalidType");
+		}
+		
+	if(c1>c2)
+		return c1-c2;
+	else
+		if(c1<c2)
+			return c2-c1;
+		else
+			if(c1==0 && c2==0)
+				return 0;
+}
 
 module.exports.distance = distance
